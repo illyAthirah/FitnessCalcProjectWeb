b92befcf674f9f5ea1f4f015cbaa34a7bf4f3716
haru0l <86917622+haru0l@users.noreply.github.com>
2024-06-03 20:35:03 +0800

    meow

--- build/generated/jax-wsCache/MyServer_1/com/cal/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://basicCalc.com/")
-package com.cal;
--- build/generated/jax-wsCache/MyServer_3/com/fitnessclient/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://fitnesscalc.com/")
-package com.fitnessclient;
