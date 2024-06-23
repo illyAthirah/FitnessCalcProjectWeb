8e696033aaa21d07603ea7e101b9a51579b8af2e
illyAthirah <86216356+illyAthirah@users.noreply.github.com>
2024-06-23 22:28:55 +0800

    fault message

--- src/java/com/fitnessCalc/FitnessCalcWS.java
+++ src/java/com/fitnessCalc/FitnessCalcWS.java
@@ -41,7 +41,27 @@ public class FitnessCalcWS {
     }
     
     @WebMethod(operationName = "displayInformation")
-    public String displayInformation(String fullName, String icNum, String gender, double weight, double height) {
+    public String displayInformation(String fullName, String icNum, String gender, double weight, double height) throws FaultMessage {
+        if (fullName == null || fullName.isEmpty()) {
+            throw new FaultMessage("Full Name cannot empty");
+        }
+        
+        if (icNum == null || icNum.isEmpty()) {
+            throw new FaultMessage("IC Number cannot empty");
+        }
+        
+        if (gender == null || gender.isEmpty()) {
+            throw new FaultMessage("Gender cannot empty");
+        }
+        
+        if (weight <= 0) {
+            throw new FaultMessage("Weight must be greater than zero");
+        }
+        
+        if (height <= 0) {
+            throw new FaultMessage("Height must be greater than zero");
+        }
+        
     return "<br />Full Name: " + fullName + "<br />" +
                "IC Number: " + icNum + "<br />" +
                "Gender: " + gender + "<br />" +
@@ -50,13 +70,19 @@ public class FitnessCalcWS {
     }
     
     @WebMethod(operationName = "determineBMI")
-    public String determineBMI(double weight, double height) {
+    public String determineBMI(double weight, double height) throws FaultMessage {
+        if (weight <= 0 || height <= 0) {
+            throw new FaultMessage("Weight and Height must be greater than zero");
+        }
         double bmi = (weight / (height * height)) * 10000;
         return String.format("%.2f", bmi);
     }
     
-     public String bodyFat(double bmi, int age, String gender) {
-      double bodyFatPercentage;
+     public String bodyFat(double bmi, int age, String gender)throws FaultMessage {
+      if (bmi <= 0 || age <= 0) {
+            throw new FaultMessage("BMI and Age must be greater than zero");
+        }
+         double bodyFatPercentage;
       if (gender.equals("Male"))
           bodyFatPercentage = (1.20 * bmi) + (0.23 * age) - 16.2;
       else
@@ -64,13 +90,19 @@ public class FitnessCalcWS {
       return String.format("%.2f", bodyFatPercentage);
   }
      
-     public String calorieBurnRate(int duration, double met, double weight) {
-      double burnRate = (duration*met*weight)/200;
+     public String calorieBurnRate(int duration, double met, double weight) throws FaultMessage{
+          if (duration <= 0 || met <= 0 || weight <= 0) {
+            throw new FaultMessage ("Duration, MET, and Weight must be greater than zero");
+        }
+         double burnRate = (duration*met*weight)/200;
       return String.format("%.2f", burnRate);
   }
      
-     public String dailyCalorieNeed(double weight, double height, int age, String gender) {
-      double caloryNeed;
+     public String dailyCalorieNeed(double weight, double height, int age, String gender) throws FaultMessage {
+      if (weight <= 0 || height <= 0 || age <= 0) {
+            throw new FaultMessage("Weight, Height, and Age must be greater than zero");
+        }
+         double caloryNeed;
       
       if (gender.equals("Male"))
           caloryNeed = (13.397 * weight) + (4.799 * height) - (5.677 * age) + 88.362;
